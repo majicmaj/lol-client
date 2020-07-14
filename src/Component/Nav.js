0ae40a3db114faae1808900b4eba4915193ccec8
Majicmaj <52975556+majicmaj@users.noreply.github.com>
2020-07-14 17:29:33 -0400

    Updated Navbar

--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -1,29 +1,38 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../img/logo.svg";
 import "../App.css";
 
 class Nav extends Component {
-    render() {
-        return <nav className="navbar">
-            <div className="leftnav">
-                <Link to="/">
-                    <div className="logo" />
-                </Link>
-            </div>
+  render() {
+    return (
+      <nav className="navbar">
+        <a className="logo" href="https://www.majdtarbin.com" target="_blank">
+          <Logo className="svg" />
+        </a>
 
-            <div className="rightnav">
-                <Link to="/builder">
-                    <h3>Builder</h3>
-                </Link>
-                <Link to="/teams">
-                    <h3>Teams</h3>
-                </Link>
-                <Link to="/champions">
-                    <h3>Champions</h3>
-                </Link>
-            </div>
-        </nav>
-    }
+        <div className="links">
+          <Link className="link" to="/builder">
+            Builder
+          </Link>
+          <Link className="link" to="/teams">
+            Teams
+          </Link>
+          <Link className="link" to="/champions">
+            Champions
+          </Link>
+        </div>
+        <a className="kofi" href="https://ko-fi.com/Z8Z01WU4W" target="_blank">
+          <img
+            height="36"
+            src="https://cdn.ko-fi.com/cdn/kofi3.png?v=2"
+            border="0"
+            alt="Buy Me a Coffee at ko-fi.com"
+          />
+        </a>
+      </nav>
+    );
+  }
 }
 
 export default Nav;
